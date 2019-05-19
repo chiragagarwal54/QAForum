07c374602ef2eac2352fe89d95a21a9fa115f4a7
Chirag Agarwal <chiragagarwal54@yahoo.com>
2019-05-19 22:50:01 +0530

    added 2 models, made home page

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class question(models.Model):
+    ques = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.ques
+
+class answer(models.Model):
+    ans = models.CharField(max_length=5000)
